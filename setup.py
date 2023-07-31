721b39d319a97444b795a630422e3922c611de41
riccardocadei <riccardo.cadei98@gmail.com>
2023-07-31 16:29:34 -0400

    update setup

--- setup.py
+++ setup.py
@@ -7,20 +7,25 @@ with open("README.md", "r") as fh:
 setup(
     name="pycre", 
     version="0.0.1",
-    author="Riccardo Cadei", "Naeem Khoshnevis", "Falco Joannes Bargagli Stoffi"
-    author_email="rcadei@hsph.harvard.edu", "nkhoshnevis@g.harvard.edu", "fbargaglistoffi@hsph.harvard.edu"
+    author=["Riccardo Cadei", "Naeem Khoshnevis", 
+        "Falco Joannes Bargagli Stoffi"],
+    author_email=["rcadei@hsph.harvard.edu", 
+        "nkhoshnevis@g.harvard.edu", 
+        "fbargaglistoffi@hsph.harvard.edu"],
     maintainer="Naeem Khoshnevis",
     maintainer_email="nkhoshnevis@g.harvard.edu",
-    description="Python implementation of Causal Rule Ensemble algorithm.",
-    long_description="Provides a new method for interpretable heterogeneous 
-        treatment effects characterization in terms of decision rules 
-        via an extensive exploration of heterogeneity patterns by an 
-        ensemble-of-trees approach, enforcing high stability in the 
-        discovery. It relies on a two-stage pseudo-outcome regression, and 
-        theoretical convergence guarantees support it. Bargagli-Stoffi, 
-        F. J., Cadei, R., Lee, K., & Dominici, F. (2023) Causal rule ensemble: 
-        Interpretable Discovery and Inference of Heterogeneous Treatment Effects.  
-        arXiv preprint <arXiv:2009.09036>.",
+    description="""Python implementation of Causal Rule Ensemble 
+        algorithm.""",
+    long_description="""Provides a new method for interpretable 
+        heterogeneous treatment effects characterization in terms of 
+        decision rules via an extensive exploration of heterogeneity 
+        patterns by an ensemble-of-trees approach, enforcing high 
+        stability in the discovery. It relies on a two-stage 
+        pseudo-outcome regression, and theoretical convergence 
+        guarantees support it. Bargagli-Stoffi, F. J., Cadei, R., 
+        Lee, K., & Dominici, F. (2023) Causal rule ensemble: 
+        Interpretable Discovery and Inference of Heterogeneous 
+        Treatment Effects. arXiv preprint <arXiv:2009.09036>.""",
     long_description_content_type="text/markdown",
     url="TBD",
     license="GPL-3",
